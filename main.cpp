f51cc3b6d212e92ef8fb7acd53dcaf62275394f0
Anthony-Rodriguez18 <85749042+Anthony-Rodriguez18@users.noreply.github.com>
2021-12-06 04:39:38 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//Realizado en Replit
+/*Grupo: Las chicas super poderosas        CIENCIA DE LA COMPUTACIÓN I  CCOMP2-1
+INTEGRANTES: 
+- Josue Carpio 
+- Ariana Leon 
+- Anthony Rodriguez */
+
+#include "include/liberman.h"
+
+int main()
+{   
+  Menu inicio;
+  inicio.cargarDatos();
+  inicio.menuPrincipal();
+  
+  return 0;
+} 
